1d899309fa22df9e69cb8c656a05dc5411ca0f74
mzazakeith <mzazakeith@gmail.com>
2018-06-08 10:40:58 +0300

    added function for adding a new user

--- login.py
+++ login.py
@@ -2,8 +2,24 @@ class User:
     '''
     This is a class for the user
     '''
+    def create_user(self):
+
+        '''
+        Function that allows one to create new users
+        '''
+
+        print("Enter the username that you would like to use \n")
+        new_user  = input("Your Username: ")
+        print("Enter a password")
+        new_pass = getpass.getpass("Password: ")
+        with open("created.txt", "a")as myfile:
+            myfile.write("\n"+new_user + "|" + new_pass)
+            return(new_user + "|" + new_pass)
+
+user = User()
+
 
 class Credentials:
     '''
     This is a class for the Credentials
-    '''    
+    '''
