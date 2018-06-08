35c7dbf3e247962049eb93db5eba2e4df773b35b
mzazakeith <mzazakeith@gmail.com>
2018-06-08 10:43:49 +0300

    added function for adding credentials

--- login.py
+++ login.py
@@ -4,9 +4,9 @@ class User:
     '''
     def create_user(self):
 
-        '''
-        Function that allows one to create new users
-        '''
+        # '''
+        # Function that allows one to create new users
+        # '''
 
         print("Enter the username that you would like to use \n")
         new_user  = input("Your Username: ")
@@ -23,3 +23,26 @@ class Credentials:
     '''
     This is a class for the Credentials
     '''
+    def add_credentials(self):
+
+        # '''
+        # Function that allows the user to add credentials
+        # '''
+
+        print("Please re-enter your username to confirm this action")
+        user = input("username: ")
+        f = open("login.txt", "r")
+        for line in f.readlines():
+            us, pw = line.strip().split("|")
+            if (user in us):
+                print("Username Confirmed")
+                print("\n")
+                print("Enter credentials")
+                site_name = input("Enter site: ")
+                password = input("existing password: ")
+                with open(f"{user}", "a")as myfile:
+                    myfile.write(site_name + ":" + password + "\n")
+                    return(site_name + ":" + password)
+                return True
+        print("Wrong username")
+        return False
