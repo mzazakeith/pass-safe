145de9a96908fcd1184b160f51ad55dc29a09be1
mzazakeith <mzazakeith@gmail.com>
2018-06-08 10:39:41 +0300

    added test for creating a new user

--- login_test.py
+++ login_test.py
@@ -1,3 +1,10 @@
 import unittest
 from login import User
 from login import Credentials
+
+class TestUser(unittest.TestCase):
+
+    def test_create_new_user(self):
+
+        line = "user|passwd"
+        self.assertEqual(line,User.create_user(self))
